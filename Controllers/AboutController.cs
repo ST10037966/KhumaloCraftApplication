95196f14610265939e0b783378bc4650d925ae02
cameroncolley <103643997+ST10037966@users.noreply.github.com>
2024-04-02 22:17:16 +0200

    Add project files.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhumaloCraftApplication.Controllers
+{
+    public class AboutController : Controller // Inherit from Controller class
+    {
+        // GET: /About
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MyWorkController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhumaloCraftApplication.Controllers
+{
+    public class MyWorkController : Controller // Inherit from Controller class
+    {
+        // GET: /MyWork
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
